5a3eb4f3f980f2608489b54ab2460060b3719604
cefariron <adidaswww17@gmail.com>
2023-11-05 12:17:17 +0200

    complete task-08

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,22 @@
+const form = document.querySelector(".login-form")
+
+form.addEventListener("submit", handleSubmit)
+
+function handleSubmit(event) {
+    event.preventDefault()
+
+    const { email, password} = event.currentTarget.elements;
+
+    if(!email.value || !password.value){
+        alert("All fields must be filled!")
+    } else {
+        const info = {
+            email: email.value,
+            password: password.value,
+        }
+    console.log(info);
+    form.reset();
+    }
+}
+
 
