23abfc4e0edf7adbc716758a00a910f2690410a8
cefariron <adidaswww17@gmail.com>
2023-11-05 12:46:10 +0200

    complete task-09

--- js/task-09.js
+++ js/task-09.js
@@ -1,5 +1,16 @@
+const body = document.querySelector("body")
+const btn = document.querySelector(".change-color")
+const span = document.querySelector(".color")
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
+}
+
+btn.addEventListener("click", handleClick)
+
+function handleClick() {
+  body.setAttribute("style", `background-color: ${getRandomHexColor()}`)
+  span.textContent = getRandomHexColor();
 }
