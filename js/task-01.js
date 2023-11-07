274a4c0b3f5bb893c2aa279edcea691e44a68271
cefariron <adidaswww17@gmail.com>
2023-11-07 12:52:47 +0200

    Fix task-01

--- js/task-01.js
+++ js/task-01.js
@@ -8,10 +8,8 @@ console.log("Number of categories:", ctgItem.length);
 // H2 and list number
 
 ctgItem.forEach((item) => {
-    const headingText = item.querySelector("h2").textContent;
+    const headingText = item.firstElementChild.textContent;
     console.log("Category:", headingText);
-    const itemList = item.querySelectorAll("li");
-    console.log("Elements:", itemList.length);
+    const itemList = item.lastElementChild.children.length;
+    console.log("Elements:", itemList);
 })
-
-
